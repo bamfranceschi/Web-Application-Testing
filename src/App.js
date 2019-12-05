38f927f44641a748378ad37a19479fab3a147ec6
bamfranceschi <anna.franceschi@gmail.com>
2019-12-05 18:57:24 -0500

    finished tests for all functions

--- src/App.js
+++ src/App.js
@@ -1,8 +1,42 @@
 import React, { useState } from "react";
 import Dashboard from "./components/Dashboard";
 import Display from "./components/Display";
-
 import "./App.css";
+
+//simple increment function
+export const addHit = ({ hit, setHit }) => {
+  debugger;
+  return setHit(hit + 1);
+};
+
+// increment until 4 then reset to 0
+
+export const addBall = ({ ball, setBall }) => {
+  if (ball < 4) {
+    setBall(ball + 1);
+  } else {
+    setBall(0);
+  }
+};
+
+// increment until 3 then reset to 0
+
+export const addStrike = ({ strike, setStrike }) => {
+  if (strike < 3) {
+    setStrike(strike + 1);
+  } else {
+    setStrike(0);
+  }
+};
+
+//fouls increase strike count by 1, only if strike count is less than 2
+
+export const addFoul = ({ foul, setFoul, strike, setStrike }) => {
+  if (strike < 3) {
+    setFoul(foul + 1);
+    setStrike(strike + 1);
+  }
+};
 
 function App() {
   const [strike, setStrike] = useState(0);
@@ -10,75 +44,16 @@ function App() {
   const [foul, setFoul] = useState(0);
   const [hit, setHit] = useState(0);
 
-  //need logic to calculate hits
-  //simple increment function
-  const addHit = () => {
-    return setHit(hit + 1);
-  };
-
-  //need logic to calculate balls
-  // increment until 4 then reset to 0
-
-  const addBall = () => {
-    if (ball < 4) {
-      setBall(ball + 1);
-    } else {
-      setBall(0);
-    }
-  };
-
-  //need logic to calculate strikes
-  // increment until 3 then reset to 0
-
-  const addStrike = () => {
-    if (strike < 3) {
-      setStrike(strike + 1);
-    } else {
-      setStrike(0);
-    }
-  };
-
-  //need logic to calculate fouls
-  //fouls increase strike count by 1, only if strike count is less than 2
-
-  // const addFirstFoul = () => {
-  //   if (strike === 0) {
-  //     setFoul(foul + 1);
-  //   }
-  //   return setStrike(1);
-  // };
-
-  // const addSecondFoul = () => {
-  //   if (strike === 1) {
-  //     setFoul(foul + 1);
-  //   }
-  //   return setStrike(2);
-  // };
-
-  // const addMoreFouls = () => {
-  //   if (strike === 2) {
-  //     setFoul(0);
-  //   }
-  //   return foul;
-  // };
-
-  const addFoul = () => {
-    if (strike < 3) {
-      setFoul(foul + 1);
-      setStrike(strike + 1);
-    }
-  };
-
   //todo: need to import both components, need to pass down functions and state
 
   return (
     <div className="App">
       <Display balls={ball} strikes={strike} />
       <Dashboard
-        handleAddHit={addHit}
-        handleAddBall={addBall}
-        handleAddStrike={addStrike}
-        handleAddFoul={addFoul}
+        handleAddHit={() => addHit({ hit, setHit })}
+        handleAddBall={() => addBall({ ball, setBall })}
+        handleAddStrike={() => addStrike({ strike, setStrike })}
+        handleAddFoul={() => addFoul({ foul, setFoul, strike, setStrike })}
       />
     </div>
   );
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,41 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render } from "@testing-library/react";
+import App, { addHit, addBall, addFoul, addStrike } from "./App";
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+//test for addHit
+
+test("increments hit count by 1", () => {
+  const setHit = jest.fn();
+  addHit({ hit: 3, setHit });
+
+  expect(setHit).toBeCalledWith(4);
+});
+
+//test for addBall
+
+test("increments ball count by 1 until 4, then resets", () => {
+  const setBall = jest.fn();
+  addBall({ ball: 4, setBall });
+
+  expect(setBall).toBeCalledWith(0);
+});
+
+//test for addStrike
+
+test("increments strike count until 3, then resets", () => {
+  const setStrike = jest.fn();
+  addStrike({ strike: 3, setStrike });
+
+  expect(setStrike).toBeCalledWith(0);
+});
+
+//test for addFoul
+
+test("fouls increase strike count by 1, only if strike count is less than 2", () => {
+  const setFoul = jest.fn();
+  const setStrike = jest.fn();
+  addFoul({ strike: 1, foul: 1, setFoul, setStrike });
+
+  expect(setFoul).toBeCalledWith(2);
+  expect(setStrike).toBeCalledWith(2);
 });
